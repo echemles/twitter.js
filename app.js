2e666dc29691a75d280081f3897dacc5df394c03
lesterhm <lesterechem@gmail.com>
2015-09-22 18:17:48 -0400

    applied nodemon, morgan, & start script

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var express = require( 'express' );
 var app = express();
+var chalk = require('chalk');
+var morgan = require('morgan');
+
+app.use(morgan());
 
 app.get('/', function(request, response){
   console.log(response.statusCode)
-  response.send('hello there')
+  response.send('hello there!')
 })
+
+app.use('/test', function(request, response){
+  // console.log(request.method, request.route.path, response.statusCode);
+  response.send('hey will!!');
+})
+
+
 
 app.listen(3000);
